46196796309cd8f2e2a2a7ca915b88feeedb6720
Yaniv Kleinman <yaniv.kleinman@gmail.com>
2020-11-12 14:52:12 +0000

    Completed all work

--- main.py
+++ main.py
@@ -8,20 +8,19 @@ print(logo)
 
 game_over = False
 while not game_over:
-    #STEP-2: Ask for name input 
+    #STEP-2: Ask for name input
     name = input("What is your name? ")
 
-    #STEP-3: Ask for bid price 
+    #STEP-3: Ask for bid price
     bid_price = int(input("What is your bid? $"))
 
     def activate_bidder(user_name, bid):
-        #STEP-4: Add name and bid into a dictionary as the key and value. 
+        #STEP-4: Add name and bid into a dictionary as the key and value.
         user_bid[name] = bid_price
 
     activate_bidder(user_name=name, bid=bid_price)
 
-
-#STEP-5: 
+    #STEP-5:
     other_bidders = input("Are there any other bidders? Type 'yes or 'no'\n")
 
     if other_bidders == "yes":
@@ -34,5 +33,4 @@ while not game_over:
             if final_bid > winning_bid:
                 winning_bid = final_bid
         print(f"The winner is {user} with a bid of ${winning_bid}")
-        game_over = True 
-
+        game_over = True
